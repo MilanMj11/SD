879347a620d1622a76f57a83e973cd6f42d90618
MilanMj11 <mujdar.milan@gmail.com>
2023-03-18 19:01:01 +0200

    update

--- Tema1SD.cpp
+++ Tema1SD.cpp
@@ -10,20 +10,23 @@ ofstream fout("output.txt");
 const int NMAX = (1<<17)+1;
 const int INF = (1<<29);
 int a[NMAX],n;
-vector<int> bucket[NMAX];
+list<int> bucket[NMAX];
 
 
 ///____________________________________________
 void concat(int a[],int st,int dr){
-    int b[NMAX],c[NMAX];
-    int mij=(st+dr)/2;
+    int mij = (st+dr)/2;
+    int dim1 = mij-st+1;
+    int dim2 = dr-mij;
+
+    int *b = new int[dim1+5];
+    int *c = new int[dim2+5];
+
     int k=0;
-    for(int i=st;i<=mij;i++)
-        b[++k] = a[i];
+    for(int i=st;i<=mij;i++) b[++k] = a[i];
     b[++k]=INF;
     k=0;
-    for(int i=mij+1;i<=dr;i++)
-        c[++k] = a[i];
+    for(int i=mij+1;i<=dr;i++) c[++k] = a[i];
     c[++k]=INF;
     int ind1=1,ind2=1;
     k=st-1;
@@ -40,10 +43,12 @@ void concat(int a[],int st,int dr){
             continue;
         }
     }
+    delete[] b;
+    delete[] c;
 }
 
 void merge_sort(int a[],int st,int dr){
-    if(st==dr) return;
+    if(st>=dr) return;
     int mij=(st+dr)/2;
     merge_sort(a,st,mij);
     merge_sort(a,mij+1,dr);
@@ -75,7 +80,7 @@ void quicksort(int a[],int low,int high){
 ///______________________________________________
 
 ///_____________________________________________
-void RadixSort(int a[],int base){
+void RadixSort(int a[],int n,int base){
     long long p = 1;
     long long maxim = 0;
     for(int i=1;i<=n;i++){
@@ -85,13 +90,13 @@ void RadixSort(int a[],int base){
     while(p<=maxim){
         for(int i=1;i<=n;i++){
             int cifra = (a[i] / p) % base;
-            bucket[cifra].insert(bucket[cifra].begin(),a[i]);
+            bucket[cifra].push_back(a[i]);
         }
         int ind = 0;
         for(int i=0;i<base;i++){
             while(bucket[i].size()!=0){
-                a[++ind]=bucket[i].back();
-                bucket[i].pop_back();
+                a[++ind]=bucket[i].begin();
+                bucket[i].erase(pop_back();
             }
         }
         p*=base;
@@ -100,7 +105,7 @@ void RadixSort(int a[],int base){
 ///______________________________________________
 
 ///______________________________________________
-void InsertionSort(int a[]){
+void InsertionSort(int a[],int n){
     int key=0;
     for(int i=1;i<=n;i++){
         key = a[i];
@@ -115,6 +120,7 @@ void InsertionSort(int a[]){
 }
 ///______________________________________________
 
+///______________________________________________
 void ShellSort(int a[],int n){
     for(int gap = n/2; gap >= 1; gap /= 2){
         for(int i = gap; i < n; i++){
@@ -128,7 +134,10 @@ void ShellSort(int a[],int n){
         }
     }
 }
+///______________________________________________
 
+
+///______________________________________________
 void heapify_arb(int a[],int n,int node){
     if(n==0) return; /// masura precautie
     int maxim = node;
@@ -154,13 +163,72 @@ void HeapSort(int a[],int n){
         /// e deja fixat
     }
 }
+///______________________________________________
 
-void Calculate_MergeSort_Time(int a[]){
+long long Calculate_MergeSort_Time(int a[],int n){
+    int aux[NMAX];
+    for(int i=1;i<=n;i++) aux[i] = a[i];
     auto start = high_resolution_clock::now();
     merge_sort(a,1,n);
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop-start);
-    fout << duration.count() << " microseconds" << '\n';
+    for(int i=1;i<=n;i++) a[i] = aux[i];
+    return duration.count();
+}
+
+long long Calculate_QuickSort_Time(int a[],int n){
+    int aux[NMAX];
+    for(int i=1;i<=n;i++) aux[i] = a[i];
+    auto start = high_resolution_clock::now();
+    quicksort(a,1,n);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start);
+    for(int i=1;i<=n;i++) a[i] = aux[i];
+    return duration.count();
+}
+
+long long Calculate_Radix_Time(int a[],int n,int base){
+    int aux[NMAX];
+    for(int i=1;i<=n;i++) aux[i] = a[i];
+    auto start = high_resolution_clock::now();
+    RadixSort(a,n,base);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start);
+    for(int i=1;i<=n;i++) a[i] = aux[i];
+    return duration.count();
+}
+
+long long Calculate_InsertionSort_Time(int a[],int n){
+    int aux[NMAX];
+    for(int i=1;i<=n;i++) aux[i] = a[i];
+    auto start = high_resolution_clock::now();
+    InsertionSort(a,n);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start);
+    for(int i=1;i<=n;i++) a[i] = aux[i];
+    return duration.count();
+}
+
+long long Calculate_ShellSort_Time(int a[],int n){
+    int aux[NMAX];
+    for(int i=1;i<=n;i++) aux[i] = a[i];
+    auto start = high_resolution_clock::now();
+    ShellSort(a,n);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start);
+    for(int i=1;i<=n;i++) a[i] = aux[i];
+    return duration.count();
+}
+
+long long Calculate_HeapSort_Time(int a[],int n){
+    int aux[NMAX];
+    for(int i=1;i<=n;i++) aux[i] = a[i];
+    auto start = high_resolution_clock::now();
+    HeapSort(a,n);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop-start);
+    for(int i=1;i<=n;i++) a[i] = aux[i];
+    return duration.count();
 }
 
 int main() {
@@ -168,13 +236,25 @@ int main() {
     for(int i=1;i<=n;i++){
         fin >> a[i];
     }
-    ///Calculate_MergeSort_Time(a);
+    long long time_merge = Calculate_MergeSort_Time(a,n);
+    long long time_quick = Calculate_QuickSort_Time(a,n);
+    long long time_radix = Calculate_Radix_Time(a,n,(1<<16));
+    long long time_insertion = Calculate_InsertionSort_Time(a,n);
+    long long time_shell = Calculate_ShellSort_Time(a,n);
+    long long time_heap = Calculate_HeapSort_Time(a,n);
+    fout << time_merge << '\n';
+    fout << time_quick << '\n';
+    fout << time_radix << '\n';
+    fout << time_insertion << '\n';
+    fout << time_shell << '\n';
+    fout << time_heap << '\n';
     ///merge_sort(a,1,n);
     ///quicksort(a,1,n);
-    ///RadixSort(a,(1<<16));
-    ///InsertionSort(a);
+    ///RadixSort(a,n,(1<<16));
+    ///InsertionSort(a,n);
     ///ShellSort(a,n);
     ///HeapSort(a,n);
+
     for(int i=1;i<=n;i++){
         fout << a[i] << ' ';
     }
